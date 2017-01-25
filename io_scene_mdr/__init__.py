d228b78cd93c03b1a23a20b05ad49afb87c6ddc3
Stan Bobovych <stan.bobovych@gmail.com>
2017-01-24 22:03:44 -0500

    Version bump from 0.1 to 0.9

--- io_scene_mdr/__init__.py
+++ io_scene_mdr/__init__.py
@@ -21,7 +21,7 @@
 bl_info = {
     "name": "Combat Mission MDR format",
     "author": "Stanislav Bobovych",
-    "version": (0, 0, 1),
+    "version": (0, 9, 0),
     "blender": (2, 77, 0),
     "location": "File > Import-Export",
     "description": "Import-Export MDR, Import MDR mesh, UV's, materials and textures",
